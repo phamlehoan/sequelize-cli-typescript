15a87cba3e5b515bcbb42521de8e67ada84d62f7
Phạm Lê Hoàn <38832007+phamlehoan@users.noreply.github.com>
2023-11-11 07:35:41 +0700

    Update model.ts

--- src/assets/models/model.ts
+++ src/assets/models/model.ts
@@ -54,8 +54,8 @@ export interface <%= name[0].toUpperCase() + name.substr(1) %>Instance {
   }) %>
 }
 
-export = (sequelize: Sequelize, DataTypes: DataTypes) => {
-  var <%= name %> = sequelize.define('<%= name %>', {
+export default (sequelize: Sequelize, DataTypes: DataTypes) => {
+  const <%= name %> = sequelize.define('<%= name %>', {
     <% attributes.forEach(function(attribute, index) { %><%= attribute.fieldName %>: DataTypes.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(DataTypes.${attribute.dataType.toUpperCase()})` : attribute.dataType.toUpperCase() %><%= (Object.keys(attributes).length - 1) > index ? ',' : '' %><% }) %>
   }<%= underscored ? ', { underscored: true }' : '' %>);
 
